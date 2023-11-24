6c792cc68d3edab312b5043b5baf9fdba5cfe2d6
Sergii-Drozdiuk <126243923+Sergii-Drozdiuk@users.noreply.github.com>
2023-11-24 17:00:04 +0000

    Deploying to gh-pages from @ Sergii-Drozdiuk/Movies-finder@17d8b3c9549ebf01e7b76a37ca4e6f3e04b900df 🚀

--- assets/NotFoundPage-32fb2e35.js
+++
@@ -1,1 +0,0 @@
-import{j as e,b as t}from"./index-83514bba.js";function o(){return e.jsxs("div",{className:"text-center mt-3",children:["Sorry, this page does not exist. Please follow this link to the",e.jsx(t,{to:"/",children:e.jsx("span",{className:"text-blue-500",children:" home page"})}),"."]})}export{o as default};
